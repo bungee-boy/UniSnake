8c518817206501a9b721d15a71ce8fff1935c2b8
d3169324 <d3169324@TU46964>
2024-02-22 14:56:23 +0000

    Implemented snake movement

--- UniSnake/main.h
+++ UniSnake/main.h
@@ -1,8 +1,11 @@
 #pragma once
-#include "map.h"
+#include <cmath>
+#include <iostream>
+#include "SFML\Graphics.hpp"
 
 const unsigned int gWidth{ 1000 }, gHeight{ 1000 };  // Screen width, Screen height
 unsigned int gFps{ 60 };  // FPS
+const double pi{ 3.14159265359 };
 
 sf::RectangleShape gRect;  // Load rect class
 sf::CircleShape gCircle;  // Load circle class
